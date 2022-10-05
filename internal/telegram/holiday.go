a59bbd90b50e9763e0ddaf97a49f5832fa3cd3ca
bondar_vr <vlad49391@gmail.com>
2022-10-05 22:03:11 +0300

    fix json.Unmarshal

--- internal/telegram/holiday.go
+++ internal/telegram/holiday.go
@@ -6,31 +6,29 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"io"
 	"net/http"
+	"strings"
 	"time"
 )
 
 type Message struct {
-	Message []struct {
-		Name        string `json:"name"`
-		NameLocal   string `json:"name_local"`
-		Language    string `json:"language"`
-		Description string `json:"description"`
-		Country     string `json:"country"`
-		Location    string `json:"location"`
-		Type        string `json:"type"`
-		Date        string `json:"date"`
-		DateYear    string `json:"date_year"`
-		DateMonth   string `json:"date_month"`
-		DateDay     string `json:"date_day"`
-		WeekDay     string `json:"week_day"`
-	}
+	Name        string `json:"name"`
+	NameLocal   string `json:"name_local"`
+	Language    string `json:"language"`
+	Description string `json:"description"`
+	Country     string `json:"country"`
+	Location    string `json:"location"`
+	Type        string `json:"type"`
+	Date        string `json:"date"`
+	DateYear    string `json:"date_year"`
+	DateMonth   string `json:"date_month"`
+	DateDay     string `json:"date_day"`
+	WeekDay     string `json:"week_day"`
 }
 
 func (b *Bot) getHolidayInfo(country string, message *tgbotapi.Message) (tgbotapi.MessageConfig, error) {
 	timeT := b.parseDate(message.Date)
 	requestURL := fmt.Sprintf("https://holidays.abstractapi.com/v1/?api_key=%s&country=%s&year=%d&month=%d&day=%d",
 		b.cfg.ApiHoliday, country, timeT.Year(), timeT.Month(), timeT.Day())
-	fmt.Println(requestURL)
 
 	resp, err := http.Get(requestURL)
 	if err != nil {
@@ -38,26 +36,35 @@ func (b *Bot) getHolidayInfo(country string, message *tgbotapi.Message) (tgbotap
 	}
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
+	value, err := b.unmarshalJSON(resp)
 	if err != nil {
 		b.lgr.Fatal(err)
 	}
 
-	tmp, err := b.unmarshalJSON(body)
-	if err != nil {
-		b.lgr.Fatal(err)
-	}
-
-	b.lgr.Info(tmp)
-
-	msg := tgbotapi.NewMessage(message.Chat.ID, "лика пидор")
-
+	info := b.handleResponseForClient(value)
+	msg := tgbotapi.NewMessage(message.Chat.ID, info)
 	return msg, nil
 }
 
-func (b *Bot) unmarshalJSON(data []byte) (Message, error) {
-	var v Message
-	if err := json.Unmarshal(data, &v); err != nil {
+func (b *Bot) handleResponseForClient(tmp []Message) string {
+	var x []string
+	for _, value := range tmp {
+		x = append(x, value.Name)
+	}
+	finalString := strings.Join(x, ",\n")
+	if finalString == "" {
+		finalString = "There is no holiday in this country today :("
+	}
+	return finalString
+}
+
+func (b *Bot) unmarshalJSON(response *http.Response) ([]Message, error) {
+	var v []Message
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		b.lgr.Fatal(err)
+	}
+	if err := json.Unmarshal(body, &v); err != nil {
 		b.lgr.Fatal(err)
 	}
 	return v, nil
